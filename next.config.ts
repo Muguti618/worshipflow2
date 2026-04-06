9e2ad26e067095ef7576cd3678251294b7efd102
Muguti618 <matthewmuguti30@gmail.com>
2026-04-06 15:27:09 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    turbo: {
-      resolveAlias: {},
-    },
-  },
 };
 
 export default nextConfig;
